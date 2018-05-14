fd334b1ed88ad27fd5122240b6e9b4479d0e4309
Anthony Stephens <stephens.anthonyd@gmail.com>
2018-05-14 10:45:46 -0400

    added basic constructor tests for Person, Address, and Settings [#157557121]

--- src/ServiceHub.Person.Testing/Library/TestSuite.cs
+++ src/ServiceHub.Person.Testing/Library/TestSuite.cs
@@ -1,13 +1,38 @@
 using Xunit;
+using LM = ServiceHub.Person.Library.Models;
 
 namespace ServiceHub.Person.Testing.Library
 {
-  public class TestSuite
-  {
-    [Fact]
-    public void SampleTest()
+    public class TestSuite
     {
-      Assert.True(true);
+        [Fact]
+        public void PersonTest()
+        {
+            var expected = typeof(LM.Person);
+
+            var actual = new LM.Person();
+      
+            Assert.True(expected == actual.GetType());
+        }
+    
+        [Fact]
+        public void AddressTest()
+        {
+            var expected = typeof(LM.Address);
+
+            var actual = new LM.Address();
+
+            Assert.True(expected == actual.GetType());
+        }
+
+        [Fact]
+        public void SettingsTest()
+        {
+            var expected = typeof(LM.Settings);
+
+            var actual = new LM.Settings();
+            
+            Assert.True(expected == actual.GetType());
+        }
     }
-  }
 }
